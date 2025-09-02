235230e67ddc9ab2a0184264833286b2f059734e
Caetano Petry <caetano_petry@estundate.sesisenai.org.br>
2025-09-02 14:02:40 -0300

    Update atv12.py

--- Programação De Aplicativos/Python/atv12.py
+++ Programação De Aplicativos/Python/atv12.py
@@ -1,9 +1,7 @@
 import random
 
 numero_aleatorio = random.randint(1, 20)
-
 numero_esc = int (input("Digite um número entre 1 e 20: "))
-
 
 while numero_esc != numero_aleatorio:
 
@@ -14,7 +12,6 @@ while numero_esc != numero_aleatorio:
     elif numero_esc < numero_aleatorio:
         print("O número aleatorio é maior que o número escolhido.")
         numero_esc = int (input("Digite um número entre 1 e 20: "))
-
 
 else:
     print(f"Parabéns, você acertou o número aleatório, que era {numero_aleatorio}.")
