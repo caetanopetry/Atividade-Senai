0a6a06a7b3021319d3e4c297585314951398c870
Caetano Petry <caetano_petry@estundate.sesisenai.org.br>
2025-09-11 17:15:52 -0300

    commit

--- Programação De Aplicativos/exercicios/classes/Exercicios- 11-09-25/classealunoaprimorada.py
+++ Programação De Aplicativos/exercicios/classes/Exercicios- 11-09-25/classealunoaprimorada.py
@@ -11,9 +11,7 @@ class aluno:
 
     def media(self):
         media = sum(self.notas) / len(self.notas)
-        return (media)
-
-        
+        return (media)  
 
     def mostrarinfo(self):
         print("\n--------------------------------")
@@ -21,8 +19,6 @@ class aluno:
         print(f" Média: {self.media()}")
         print(f" Curso: {self.curso}")
         print("--------------------------------\n")
-
-
 
 nome = input ("Qual o nome do aluno?: ")
 curso = input ("Qual o nome do curso?: ")
@@ -41,31 +37,5 @@ aluno2 = aluno(nome, curso)
 aluno2.adicionarnota()
 aluno2.adicionarnota()
 aluno2.adicionarnota()
-aluno2.adicionarnota()
 
 aluno2.mostrarinfo()
-
-
-
-
-    
-
-
-
-#     def verificar_aprovacao(self):
-#         if self.nota > 10 or self.nota < 1:
-#             return f"Nota digitada inválida"
-#         else:
-#             if self.nota >=6:
-#                 return f"Aluno {self.nome} com a nota {self.nota} Aprovado"
-#             elif self.nota < 6:
-#                 return f"Aluno {self.nome} com a nota {self.nota} Reprovado"
-        
-# caetano = aluno ("Caetano", 6)
-# print(caetano.verificar_aprovacao())
-
-# henrique = aluno ("Henrique", 3)
-# print (henrique.verificar_aprovacao())
-
-# guilherme = aluno ("Guilherme", 11)
-# print(guilherme.verificar_aprovacao())
