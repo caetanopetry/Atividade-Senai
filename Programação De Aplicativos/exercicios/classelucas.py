58f28c7ccadae260212450f18b53ea2c2e26e992
CaetanoOuShxdow <86366615+CaetanoOuShxdow@users.noreply.github.com>
2025-09-17 14:37:13 -0300

    pandas

--- Programação De Aplicativos/exercicios/classelucas.py
+++ Programação De Aplicativos/exercicios/classelucas.py
@@ -8,10 +8,11 @@ class conta:
     def mostrar_saldo(self):
         
         tent_senha = input("Digite sua senha: ").strip()
-        if tent_senha == self._senha:
-            print(f"Seu saldo é {self._saldo}")
-        else:
+        while tent_senha != self._senha:
             print("Senha incorreta!")
+            tent_senha = input("Digite sua senha: ").strip()
+        else:
+            print(f"Seu saldo é: {self._saldo}")
 
     def atualizar_saldo(self, novo_saldo):
         self._saldo = novo_saldo
@@ -19,6 +20,8 @@ class conta:
 conta_bancaria_1 = conta("Caetano", 0000, "999")
 
 conta_bancaria_2 = conta("Lucas", 60, "1234")
+
+
 
 print(f"\nTítular: {conta_bancaria_1.titular}")
 conta_bancaria_1.mostrar_saldo()
